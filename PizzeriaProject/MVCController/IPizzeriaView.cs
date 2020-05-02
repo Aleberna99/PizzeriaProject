fc2a748e2b0ed781a5b84b536d0170f0ca5ce621
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 23:47:07 +0200

    Update IPizzeriaView

--- PizzeriaProject/MVCController/IPizzeriaView.cs
+++ PizzeriaProject/MVCController/IPizzeriaView.cs
@@ -6,6 +6,24 @@ namespace WinFormMVC.Controller
 {
     public interface IPizzeriaView
     {
-       
+        void SetController(PizzeriaController controller);
+        void AddInListView(IOrder menu, Drink drink, Pizza pizza);
+        void PrintCalculations(TotalPrice totalPrice, AveragePrice averagePrice, TotalQuantity totalQuantity);
+        void RemoveOnListView();
+        bool PrintReceipt(IOrder ordine, Client cliente);
+        string ReturnNameMenuSelected();
+        void CloseProgramm();
+        void ModifyOrder();
+
+        string NamePizza { get; }
+        string NameDrink { get; }
+        double PriceDrink { get; }
+
+        string NameClient { get; }
+        string SurnameClient { get; }
+        string CityClient { get; }
+        string AddressClient { get; }
+        long TelefonClient { get; }
+
     }
 }
