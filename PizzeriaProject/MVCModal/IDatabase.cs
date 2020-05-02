48e0067c2f30dec0feedbea0d8e666acfa5bc8ac
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 23:16:54 +0200

    Add IDatabase

---
+++ PizzeriaProject/MVCModal/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCModal
+{
+    public interface IDatabase
+    {
+        void SaveData(IOrder ordine, Client cliente);
+    }
+}
