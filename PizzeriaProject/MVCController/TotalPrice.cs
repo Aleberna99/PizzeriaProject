8eab42a3e9b6fe92ea922c2a2c55954e3d4385b1
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 23:04:02 +0200

    Add TotalPrice

---
+++ PizzeriaProject/MVCController/TotalPrice.cs
@@ -0,0 +1,50 @@
+﻿using MVCModal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCController
+{
+    public class TotalPrice : IVisitor
+    {
+        private double totalPizze = 0;
+        private double totalDrink = 0;
+        private double totalMenu = 0;
+
+        public void Visit(Drink drink)
+        {
+            totalDrink += drink.ReturnPrice();
+        }
+        public void Visit(Menu menu)
+        {
+            totalMenu += menu.ReturnPrice();
+        }
+        public void Visit(Pizza pizza)
+        {
+            totalPizze += pizza.ReturnPrice();
+        }
+
+        public void Reset()
+        {
+            totalPizze = 0;
+            totalDrink = 0;
+            totalMenu = 0;
+        }
+
+        public double GetTotalPizze()
+        {
+            return totalPizze;
+        }
+
+        public double GetTotalDrink()
+        {
+            return totalDrink;
+        }
+
+        public double GetTotalMenu()
+        {
+            return totalMenu;
+        }
+    }
+}
