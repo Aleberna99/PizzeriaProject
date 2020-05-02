a93e5a49741194da2ab9fe0898b724e056995b2c
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 22:54:28 +0200

    Add IVisitor

---
+++ PizzeriaProject/MVCModal/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCModal
+{
+    public interface IVisitor
+    {
+        void Visit(Pizza pizza);
+        void Visit(Drink drink);
+        void Visit(Menu menu);
+        void Reset();
+    }
+}
