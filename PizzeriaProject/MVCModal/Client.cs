6be9c29ed7e544192e259976fbdb03009027d802
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 22:39:12 +0200

    Add Client

---
+++ PizzeriaProject/MVCModal/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCModal
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public long Telefon { get; set; }
+
+        public string ReturnName()
+        {
+            return Name + " " + Surname;
+        }
+
+        public string show()
+        {
+            return "DATI CLIENTE :\nNome : " + Name + "\nCognome : " + Surname + "\nIndirizzo : " + Address  + "\nCittà : " + City + "\nTelefono : " + Telefon;
+        }
+    }
+}
