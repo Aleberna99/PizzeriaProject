e1e086d3c78d421934b7635d5b548fd186533d9c
Aleberna99 <a.bernardini18@campus.uniurb.it>
2020-05-02 23:15:08 +0200

    Add IOrderExtensions

---
+++ PizzeriaProject/MVCModal/IOrderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MVCModal
+{
+    public static class IOrderExtensions
+    {
+        public static ListViewItem ToListViewItem(this IOrder menu, Drink drink, Pizza pizza)
+        {
+            string[] row = { menu.ReturnName(), pizza.ReturnName(), drink.ReturnName(), menu.ReturnPrice().ToString() };
+            return new ListViewItem(row);
+        }
+
+        public static string ToMenu(this ListViewItem menu)
+        {
+            return menu.SubItems[0].Text;
+        }
+    }
+}
